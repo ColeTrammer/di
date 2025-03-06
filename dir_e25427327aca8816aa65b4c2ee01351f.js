2a8303e4d16c8ce6795b9c2ce7605265cfcc08a9
ColeTrammer <cole.trammer@gmail.com>
2025-03-06 07:11:38 +0000

    docs: deploy documentation site

---
+++ dir_e25427327aca8816aa65b4c2ee01351f.js
@@ -0,0 +1,7 @@
+var dir_e25427327aca8816aa65b4c2ee01351f =
+[
+    [ "compiler.h", "compiler_8h.html", "compiler_8h" ],
+    [ "custom.h", "custom_8h.html", "custom_8h" ],
+    [ "default_generic_domain.h", "default__generic__domain_8h.html", "default__generic__domain_8h" ],
+    [ "prelude.h", "platform_2prelude_8h.html", "platform_2prelude_8h" ]
+];
---
+++ navtreedata.js
@@ -0,0 +1,193 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "di", "index.html", [
+    [ "di Library", "index.html", "index" ],
+    [ "Building", "md_docs_2pages_2build.html", [
+      [ "Building with CMake", "md_docs_2pages_2build.html#autotoc_md13", [
+        [ "Prerequisites", "md_docs_2pages_2build.html#autotoc_md14", null ],
+        [ "Consuming via CMake", "md_docs_2pages_2build.html#autotoc_md15", null ],
+        [ "Manual Build Commands", "md_docs_2pages_2build.html#autotoc_md16", null ],
+        [ "Install Commands", "md_docs_2pages_2build.html#autotoc_md17", null ],
+        [ "Note to packagers", "md_docs_2pages_2build.html#autotoc_md18", null ]
+      ] ],
+      [ "Building with nix", "md_docs_2pages_2build.html#autotoc_md19", [
+        [ "Prerequisites", "md_docs_2pages_2build.html#autotoc_md20", null ],
+        [ "Consuming via Flake", "md_docs_2pages_2build.html#autotoc_md21", null ],
+        [ "Manual Build Commands", "md_docs_2pages_2build.html#autotoc_md22", null ]
+      ] ]
+    ] ],
+    [ "Developing", "md_docs_2pages_2developing.html", [
+      [ "Nix Dev Shell", "md_docs_2pages_2developing.html#autotoc_md30", [
+        [ "Note for Auto-Completion", "md_docs_2pages_2developing.html#autotoc_md31", null ]
+      ] ],
+      [ "CMake Developer Mode", "md_docs_2pages_2developing.html#autotoc_md32", [
+        [ "Presets", "md_docs_2pages_2developing.html#autotoc_md33", null ],
+        [ "Configure Build and Test", "md_docs_2pages_2developing.html#autotoc_md34", null ],
+        [ "Developer Mode Targets", "md_docs_2pages_2developing.html#autotoc_md35", [
+          [ "coverage", "md_docs_2pages_2developing.html#autotoc_md36", null ],
+          [ "docs", "md_docs_2pages_2developing.html#autotoc_md37", null ]
+        ] ]
+      ] ],
+      [ "Justfile", "md_docs_2pages_2developing.html#autotoc_md38", null ]
+    ] ],
+    [ "Library Component Overview", "md_docs_2pages_2library__component__overview.html", "md_docs_2pages_2library__component__overview" ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", "namespacemembers_dup" ],
+        [ "Functions", "namespacemembers_func.html", "namespacemembers_func" ],
+        [ "Variables", "namespacemembers_vars.html", "namespacemembers_vars" ],
+        [ "Typedefs", "namespacemembers_type.html", "namespacemembers_type" ],
+        [ "Enumerations", "namespacemembers_enum.html", null ]
+      ] ]
+    ] ],
+    [ "Concepts", "concepts.html", "concepts" ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", "functions_type" ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Related Symbols", "functions_rela.html", "functions_rela" ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ],
+    [ "GitHub", "^https://github.com/ColeTrammer/di", null ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"abs_8h.html",
+"black__box_8h.html",
+"classdi_1_1BitSet.html#a8eef687f38bbc4d97baa14293cc1d404",
+"classdi_1_1ErasedString.html#aaad44316939c5c8dba279087bdeb3235",
+"classdi_1_1IntrusiveForwardListNode.html#a0549acab79749fced1fd54b6e2489a06",
+"classdi_1_1LayoutRight_1_1Mapping.html#a6f00e0fcbf495f58126ea961b949b4c3",
+"classdi_1_1MovableBox.html#a4b8a41c0a44341d90634a51892a40c3e",
+"classdi_1_1RebindableBox.html#a45868f4eaccfff44095e483ee8a5fda3",
+"classdi_1_1StaticVector.html#a26b6cd79df89461faac18be09305956e",
+"classdi_1_1UUID.html#a7da8fecad01f4cd5df24d8056fe1ff5c",
+"classdi_1_1VectorWriter.html#ad511bd9fb71199e879aba0db80e28557",
+"classdi_1_1chrono_1_1Duration.html#a4c6e883a936f5a48ef1d4db7ab2a0ce4",
+"classdi_1_1container_1_1ChunkByView.html#a4579a29548e4c58cbda8157cab9e3441",
+"classdi_1_1container_1_1ConstantVectorInterface.html#a11d70bf5e3345aa297050fc4d6f9bdb0",
+"classdi_1_1container_1_1ErasedString.html#a363c55a9a874b230d19db36066ec4ed7",
+"classdi_1_1container_1_1IntrusiveSetInterface.html#a62e18550f366c096059fc4d61d2b74a1",
+"classdi_1_1container_1_1MapInterface.html#a5388e0f302384e68318d4f17bc9cc988",
+"classdi_1_1container_1_1NodeHashTable.html#a3c46f425e97df15b194ffc46da521062",
+"classdi_1_1container_1_1PriorityQueue.html#af2ef314d68ca77c3d0d5328fda96ebac",
+"classdi_1_1container_1_1Ring.html#a1b02708b795e98b20ee6d4eb8850a368",
+"classdi_1_1container_1_1SplitView.html#a04ed8e28005f043c429da7f62640aca7",
+"classdi_1_1container_1_1Vector.html#ac00ca4af78d988b9b3df9d63ba36f042",
+"classdi_1_1container_1_1string_1_1MutableStringInterface.html#a5dddbcdceae1bd538b64d69f925af490",
+"classdi_1_1execution_1_1AnySenderT_1_1Type.html#ab04decf3c2791b1f3850d61504aafeba",
+"classdi_1_1function_1_1detail_1_1BindBackFunction_3_01meta_1_1ListV_3_01indices_8_8_8_01_4_00_01F_00_01BoundArgs_8_8_8_01_4.html#a539213a5d2ea27035426fbc7d375ba45",
+"classdi_1_1function_1_1function__ref__ns_1_1FunctionRef_3_01Sig_00_01R_07Args_8_8_8_08_4.html#a415c2d891cd3a0963601c6bebd4eb411",
+"classdi_1_1math_1_1Rational.html#a463088978b8d4fd3a1c891fcfde56ad2",
+"classdi_1_1random_1_1LinearCongruentialEngine.html#aa2d0c1b037d8bb96b0cdc194ecddedd5",
+"classdi_1_1serialization_1_1json_1_1Value.html#a9bb4b97ce5ca0adbd5429538c1b8cd24",
+"classdi_1_1sync_1_1UniqueLock.html#a5d754736f8b2e7c1069831190c6d00d0",
+"classdi_1_1util_1_1ScopeValueChange.html#a5478792aaaea7e60a47a663f791f64bd",
+"classdi_1_1vocab_1_1Expected_3_01T_00_01E_01_4.html#a6e7d72dd40996411cf8c66b7450d48a2",
+"classdi_1_1vocab_1_1Expected_3_01void_00_01E_01_4.html#aecb2a36d8f6296b61bc570bf76c8c984",
+"classdi_1_1vocab_1_1MDSpan.html#a1dff0c7c5376fbaf4c0560cd04334bad",
+"classdi_1_1vocab_1_1Optional_3_01void_01_4.html#ab5808964acdfe109176a8c8d2dab9a7a",
+"classdi_1_1vocab_1_1Tuple.html#a28afb7072e1ab0b45bc78415171fdc22",
+"classdi_1_1vocab_1_1detail_1_1StatusCodeStorage.html#a4cca2b01399e9c29b8bfd555da492707",
+"conceptdi_1_1concepts_1_1DecayConstructible.html",
+"conceptdi_1_1concepts_1_1ParserOf.html",
+"conceptdi_1_1concepts_1_1detail_1_1InvocableToComparisonCategory.html",
+"conceptdi_1_1container_1_1view_1_1detail_1_1EmptyCartesianProduct.html",
+"container_2ring_2prelude_8h.html",
+"dir_e89c4ceeb5ee9f3c64e9072e620640b0.html",
+"format_2builtin__formatter_2tuple_8h_source.html",
+"header_8h.html",
+"just_8h.html",
+"meta_2iterator__ssize__type_8h.html",
+"namespacedi.html#a57d4fac86d6030d3ac485ff8826765bd",
+"namespacedi.html#ad7141d338b8095eb0bef8e4b2789c2cc",
+"namespacedi_1_1container.html#a09aa9b852ee6cfefb5044e762ecd6f8c",
+"namespacedi_1_1container.html#af8cbc17d91e4cca610553c7f8f75b2af",
+"namespacedi_1_1execution.html#a44c87368345e68d8b7342e32cd3b469a",
+"namespacedi_1_1execution_1_1let__each__ns.html",
+"namespacedi_1_1format_1_1style.html#ad41c0522e23a66f22c7c01e2741b29aaae90dfb84e30edf611e326eeb04d680de",
+"namespacedi_1_1math.html#a20aa034ae42e11e94946f03029986467",
+"namespacedi_1_1meta_1_1detail.html#a3921c42e528f975917744b2344ebd682",
+"namespacedi_1_1util.html#a40b816e3f0c43e72290d32d6f4e5c6b7",
+"node__hash__set_8h_source.html",
+"replace__if_8h_source.html",
+"string__iterator__at__offset_8h.html",
+"structdi_1_1Array.html#aca3d649a3a37a826e73a77105480be69",
+"structdi_1_1Curry.html#af9060ada0a4ac7347bd9ec5e5d04f4c3",
+"structdi_1_1RetainObject.html#a992f196509a67d43e202c91e10728bfa",
+"structdi_1_1bit_1_1detail_1_1BitCeilFunction.html#a971eb7f5e256a8f132818b6bccbecf2a",
+"structdi_1_1container_1_1InFoundResult.html",
+"structdi_1_1container_1_1REndFunction.html",
+"structdi_1_1container_1_1detail_1_1FindLastNotOfFunction.html",
+"structdi_1_1container_1_1detail_1_1MoveFunction.html#ab653d4544a21a71d4e3bb99629efee31",
+"structdi_1_1container_1_1detail_1_1SwapRanges.html",
+"structdi_1_1container_1_1view_1_1detail_1_1ConcatFunction.html#af7da320ff1873b054b76b35f5efabe2f",
+"structdi_1_1execution_1_1async__generator__ns_1_1AsyncGeneratorT_1_1Type.html#a2cb7ef2254af27c8edf89320a381b95a",
+"structdi_1_1execution_1_1counting__scope__ns_1_1NestSenderT_1_1Type.html#ad878347961a00de56da9654658d45915",
+"structdi_1_1execution_1_1filter__ns_1_1FinalReceiverT_1_1Type.html",
+"structdi_1_1execution_1_1first__value__ns_1_1OperationStateT_1_1Type.html#a18d89f12a223938ec2f4bc6de032ed62",
+"structdi_1_1execution_1_1into__lockstep__sequence__ns_1_1Function.html#a7d5cb14062845cb6d8dcf648f3c59531",
+"structdi_1_1execution_1_1ipc__binary__ns_1_1ReplyWaiterBase.html#abd5086da1ae4c6ae756ff6ce539cceae",
+"structdi_1_1execution_1_1let__value__with__ns_1_1Function.html#aefea3d4b295b7093fc0cd2d44dc15365",
+"structdi_1_1execution_1_1repeat__ns_1_1ReceiverT_1_1Type.html#ad6d27e42e01751fb26b5b3801f43a847",
+"structdi_1_1execution_1_1start__detached__ns_1_1DataT_1_1Type.html#a27acb98bc0974498718f4800febefbfb",
+"structdi_1_1execution_1_1when__all__ns_1_1DataT_1_1Type.html#a13578cbd8d5c7ec0702764fc946ea02c",
+"structdi_1_1execution_1_1zip__ns_1_1OuterDataT_1_1Type.html#a0e904746efbf91ead52e0ed3b4fcb2df",
+"structdi_1_1format_1_1detail_1_1VPresentEncodedFunction.html",
+"structdi_1_1function_1_1detail_1_1InvokeFunction.html#abe21c70bfdb16c0414bb4bde4bf63802",
+"structdi_1_1function_1_1monad_1_1EnableMonadFunction.html",
+"structdi_1_1meta_1_1Constexpr.html#a38092998e2a1cf0d82d588f70ef7342f",
+"structdi_1_1meta_1_1detail_1_1CommonErrorHelper_3_01T_00_01U_01_4.html",
+"structdi_1_1meta_1_1detail_1_1PopBackHelper.html",
+"structdi_1_1parser_1_1detail_1_1IntegerFunction.html",
+"structdi_1_1types_1_1ForwardIteratorTag.html",
+"structdi_1_1vocab_1_1Array.html",
+"structdi_1_1vocab_1_1DefaultDelete.html#acdc22a16cc2e5d7686350973db9cc88b",
+"take__view_8h.html",
+"vector__clear_8h.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronization';
+var SYNCOFFMSG = 'click to enable panel synchronization';
